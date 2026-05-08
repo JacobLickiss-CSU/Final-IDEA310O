0a6890d37ecc9e14e7c5e25f85f90cc543eebb63
JacobLickiss-CSU <156729396+JacobLickiss-CSU@users.noreply.github.com>
2026-05-07 23:06:01 -0600

    HUD and Level 2 geometry

--- Assets/Scripts/PlayerInterface.cs
+++ Assets/Scripts/PlayerInterface.cs
@@ -1,29 +1,77 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 using TMPro;
+using System.Collections.Generic;
 
 public class PlayerInterface : MonoBehaviour
 {
     public static PlayerInterface Instance { get; private set; }
 
-    public Image GameOverFilter;
+    public UnityEngine.UI.Image GameOverFilter;
 
     public RawImage GameOverCharacter;
 
-    public Image GameOverTextBack;
+    public UnityEngine.UI.Image GameOverTextBack;
 
     public TextMeshProUGUI GameOverText;
+
+    public UIDocument MainHUD;
+
+    private Dictionary<VisualElement, Color> baseColors = new Dictionary<VisualElement, Color>();
+
+    private Dictionary<VisualElement, Color> baseBackgroundColors = new Dictionary<VisualElement, Color>();
+
+    private Dictionary<VisualElement, Color> baseTints = new Dictionary<VisualElement, Color>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Instance = this;
+
+        SaveColors();
+    }
+
+    void SaveColors()
+    {
+        if (MainHUD != null)
+        {
+            MainHUD.rootVisualElement.Query<VisualElement>().ForEach(element =>
+            {
+                StyleColor baseColor = element.resolvedStyle.color;
+                StyleColor baseBackgroundColor = element.resolvedStyle.backgroundColor;
+                StyleColor baseTint = element.resolvedStyle.unityBackgroundImageTintColor;
+                baseColors.Add(element, new Color(baseColor.value.r, baseColor.value.g, baseColor.value.b, baseColor.value.a));
+                baseBackgroundColors.Add(element, new Color(baseBackgroundColor.value.r, baseBackgroundColor.value.g, baseBackgroundColor.value.b, baseBackgroundColor.value.a));
+                baseTints.Add(element, new Color(baseTint.value.r, baseTint.value.g, baseTint.value.b, baseTint.value.a));
+            });
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateHUD();
+    }
+
+    void UpdateHUD()
+    {
+        if (MainHUD != null && DataManager.Instance != null)
+        {
+            float healthWidth = (float)DataManager.Instance.CurrentHealth / (float)DataManager.Instance.MaxHealth;
+            var healthBarOver = MainHUD.rootVisualElement.Q<VisualElement>("HealthBarOver");
+            healthBarOver.style.width = Length.Percent(healthWidth * 100);
+
+            var healthBarUnder = MainHUD.rootVisualElement.Q<VisualElement>("HealthBarUnder");
+            healthBarUnder.style.width = Length.Percent(healthWidth * 100);
+
+            float staminaWidth = (float)DataManager.Instance.CurrentStamina / (float)DataManager.Instance.MaxStamina;
+            var staminaBar = MainHUD.rootVisualElement.Q<VisualElement>("StaminaBar");
+            staminaBar.style.width = Length.Percent(staminaWidth * 100);
+
+            var healingText = MainHUD.rootVisualElement.Q<Label>("HealCount");
+            healingText.text = ""+DataManager.Instance.CurrentHeals;
+        }
     }
 
     public void SetGameOverProgress(float progress, bool showText = true)
@@ -38,6 +86,19 @@ public class PlayerInterface : MonoBehaviour
         bool DeathCamEnabled = GameOverCharacter.color.a > 0;
         GameOverCharacter.gameObject.SetActive(DeathCamEnabled);
         DeathCamera.Instance?.gameObject.SetActive(DeathCamEnabled);
+
+        if (MainHUD != null)
+        {
+            MainHUD.rootVisualElement.Query<VisualElement>().ForEach(element =>
+            {
+                Color baseColor = baseColors[element];
+                Color baseBackgroundColor = baseBackgroundColors[element];
+                Color baseTint = baseTints[element];
+                element.style.color = new Color(baseColor.r, baseColor.g, baseColor.b, baseColor.a * (1f - progress));
+                element.style.backgroundColor = new Color(baseBackgroundColor.r, baseBackgroundColor.g, baseBackgroundColor.b, baseBackgroundColor.a * (1f - progress));
+                element.style.unityBackgroundImageTintColor = new Color(baseTint.r, baseTint.g, baseTint.b, baseTint.a * (1f - progress));
+            });
+        }
     }
 
     void SetRenderTextureDimensions()
--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -6,10 +6,15 @@ public class Sun : MonoBehaviour
 
     public float distance = 100f;
 
+    private float baseIntensity = 1.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (SunLight != null)
+        {
+            baseIntensity = SunLight.GetComponent<Light>().intensity;
+        }
     }
 
     // Update is called once per frame
@@ -19,6 +24,24 @@ public class Sun : MonoBehaviour
         {
             transform.position = PlayerManager.Instance.transform.position - SunLight.transform.forward * distance;
             transform.LookAt(PlayerManager.Instance.transform.position);
+
+            // Dim light below 0f
+            float depth = PlayerManager.Instance.transform.position.y;
+            if (depth < 0)
+            {
+                if (depth <= -10)
+                {
+                    SunLight.GetComponent<Light>().intensity = 0f;
+                }
+                else
+                {
+                    SunLight.GetComponent<Light>().intensity = baseIntensity * ((10f + depth) / 10f);
+                }
+            }
+            else
+            {
+                SunLight.GetComponent<Light>().intensity = baseIntensity;
+            }
         }
     }
 }
